0df835fe9ac6d2c6a4fa6ef9e8ddd43c172b669f
Shanaka Senevirathne <shanaka.gunasekarasenevirathne@nab.com.au>
2020-11-30 14:26:37 +1100

    Initial Commit

---
+++ node-module-cleaner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  node-module-cleaner
+//
+//  Created by Shanaka Senevirathne on 30/11/20.
+//  Copyright © 2020 Shanaka Senevirathne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
